0f132109de462a1285815ffddc3b815ddd0bf0a0
Florian Vahl <git@flova.de>
2024-10-25 02:12:42 +0200

    Working version of chunced prediction

--- ddlitlab2024/ml/train_diffusion_transformer.py
+++ ddlitlab2024/ml/train_diffusion_transformer.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn.functional as F  # noqa
+from diffusers.schedulers.scheduling_ddim import DDIMScheduler
 from diffusers.schedulers.scheduling_ddpm import DDPMScheduler
 from torch import nn
 from tqdm import tqdm
@@ -14,12 +15,12 @@ class TrajectoryTransformerModel(nn.Module):
     def __init__(self, num_joints, hidden_dim, num_layers, num_heads, max_seq_len):
         super().__init__()
         self.embedding = nn.Linear(num_joints, hidden_dim)
-        self.merge_embedding = nn.Linear(hidden_dim, hidden_dim)
-        self.positional_encoding = PositionalEncoding(hidden_dim, max_seq_len)
+        self.positional_encoding = PositionalEncoding(hidden_dim, max_seq_len + 1)
         self.step_encoding = StepEncoding(hidden_dim)
         self.transformer_decoder = nn.TransformerDecoder(
             nn.TransformerDecoderLayer(
-                d_model=hidden_dim, nhead=num_heads, dim_feedforward=hidden_dim, batch_first=True
+                d_model=hidden_dim, nhead=num_heads, dim_feedforward=hidden_dim, batch_first=True, norm_first=True, activation=
+                "gelu"
             ),
             num_layers=num_layers,
         )
@@ -29,18 +30,20 @@ class TrajectoryTransformerModel(nn.Module):
         # x shape: (batch_size, seq_len, joint, num_bins)
         # Flatten the joint and bin dimensions into a single token dimension
         x = x.view(x.size(0), x.size(1), -1)
+        # Embed the input
+        x = self.embedding(x)
+        # Add token for the step
+        x = torch.cat([self.step_encoding(step).unsqueeze(1), x], dim=1)
         # Positional encoding
-        x = self.embedding(x) + self.positional_encoding(x)
-        # Merge embedding
-        x = self.merge_embedding(x)
-        # Add a step encoding
-        x = self.step_encoding(x, step)
+        x += self.positional_encoding(x)
         # Create a causal mask
         tgt_mask = self.generate_square_subsequent_mask(x.size(1)).to(x.device)
         # Memory tensor (not used)
         memory = torch.zeros(x.size(0), 1, x.size(2)).to(x.device)
         # Pass through the transformer decoder
         out = self.transformer_decoder(x, memory, tgt_mask=tgt_mask)  # Causal mask applied
+        # Remove the step token
+        out = out[:, 1:]
         # Final classification layer (logits for each bin)
         return self.fc_out(out)
 
@@ -67,29 +70,27 @@ class PositionalEncoding(nn.Module):
 
 # Sinosoidal step encoding
 class StepEncoding(nn.Module):
-    def __init__(self, d_model):
+    def __init__(self, dim):
         super().__init__()
-        # div terms should not be trainable
-        self.div_term = torch.exp(
-            torch.arange(0, d_model, 2, requires_grad=False).float() * (-np.log(10000.0) / d_model)
-        ).to(device)
-        self.d_model = d_model
+        self.dim = dim
 
-    def forward(self, x, step):
-        x[..., 0::2] += torch.sin(step.unsqueeze(1) * self.div_term).unsqueeze(1)
-        x[..., 1::2] += torch.cos(step.unsqueeze(1) * self.div_term).unsqueeze(1)
-        return x
+    def forward(self, x):
+        half_dim = self.dim // 2
+        emb = torch.exp(torch.arange(half_dim, device=x.device) * -np.log(10000) / (half_dim - 1))
+        emb = x[:, None] * emb[None, :]
+        emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
+        return emb
 
 
 # Define dimensions for the Transformer model
 trajectory_dim = 1  # 1D input for the sine wave
-hidden_dim = 512
-num_layers = 2
+hidden_dim = 256
+num_layers = 1
 num_heads = 4
 sequence_length = 30
 
 # Generate a dataset of sine wave trajectories (500 samples)
-num_samples = 10000
+num_samples = 5000
 time = torch.linspace(0, 2 * np.pi, sequence_length).unsqueeze(-1).to(device)
 real_trajectories = torch.sin(time + torch.rand(1, num_samples).to(device) * 2 * np.pi).permute(1, 0).to(device)
 
@@ -115,15 +116,18 @@ model = TrajectoryTransformerModel(
     max_seq_len=sequence_length,
 ).to(device)
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
-lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=1e-3, total_steps=epochs * (num_samples // 32))
+lr = 1e-3
 
-scheduler = DDPMScheduler()
+optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+lr_scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=lr, total_steps=epochs * (num_samples // batch_size))
+
+scheduler = DDPMScheduler(beta_schedule="squaredcos_cap_v2")
 scheduler.config.num_train_timesteps = 1000
 
 
 # Training loop
 for epoch in tqdm(range(epochs)):  # Number of training epochs
+    mean_loss = 0
     for batch in range(num_samples // batch_size):
         targets = real_trajectories[batch * batch_size : (batch + 1) * batch_size].to(device)
 
@@ -154,44 +158,44 @@ for epoch in tqdm(range(epochs)):  # Number of training epochs
         # Compute the loss
         loss = F.mse_loss(predicted_bins, noise)
 
+        mean_loss += loss.item()
+
         # Backpropagation and optimization
         loss.backward()
         optimizer.step()
         lr_scheduler.step()
 
     if epoch % 2 == 0:
-        print(f"Epoch {epoch}, Loss: {loss.item()}")
+        print(f"Epoch {epoch}, Loss: {mean_loss / (num_samples // batch_size)}, LR: {lr_scheduler.get_last_lr()[0]}")
 
 
 # Sampling a new trajectory after training
-def sample_trajectory(length=sequence_length, diffusion_steps=1000):
-    sampled_trajectory = torch.randn(1, length, 1).to(device)
-
+def sample_trajectory(length=sequence_length, step_size=5, diffusion_steps=20):
     scheduler.set_timesteps(diffusion_steps)
 
-    for step in range(diffusion_steps):
-        with torch.no_grad():
-            t = scheduler.timesteps[step]
+    context = torch.zeros(1, 0, 1).to(device)
 
-            # latent_model_input = scheduler.scale_model_input(latent_model_input, t)
+    for _ in range(length // step_size):
 
-            # Predict the noise residual
-            noise_pred = model(sampled_trajectory, torch.tensor([t], device=device))
+        sampled_trajectory = torch.randn(1, step_size, 1).to(device)
 
-            # Normally we'd rely on the scheduler to handle the update step:
-            sampled_trajectory = scheduler.step(noise_pred, t, sampled_trajectory).prev_sample
+        for t in scheduler.timesteps:
+            with torch.no_grad():
+                print(t)
 
-            # Instead, let's do it ourselves:
-            # prev_t = max(1, t.item() - (scheduler.config.num_train_timesteps//diffusion_steps)) # t-1
-            # alpha_t = scheduler.alphas_cumprod[t.item()]
-            # alpha_t_prev = scheduler.alphas_cumprod[prev_t]
-            # predicted_x0 = (sampled_trajectory - (1-alpha_t).sqrt()*noise_pred) / alpha_t.sqrt()
-            # direction_pointing_to_xt = (1-alpha_t_prev).sqrt()*noise_pred
-            # sampled_trajectory = alpha_t_prev.sqrt()*predicted_x0 + direction_pointing_to_xt
+                sample_trajectory_with_context = torch.cat([context, sampled_trajectory], dim=1)
+
+                # Predict the noise residual
+                noise_pred = model(sample_trajectory_with_context, torch.tensor([t], device=device))[:, -step_size:]
+
+                # Normally we'd rely on the scheduler to handle the update step:
+                sampled_trajectory = scheduler.step(noise_pred, t, sampled_trajectory).prev_sample
+
+        context = torch.cat([context, sampled_trajectory], dim=1)
 
     # Plot the sampled trajectory
     plt.figure(figsize=(12, 6))
-    plt.plot(time.cpu(), sampled_trajectory[0].cpu(), label="Sampled Trajectory")
+    plt.plot(time.cpu(), context[0].cpu(), label="Sampled Trajectory")
     plt.title("Sampled Sine Wave Trajectory")
     plt.xlabel("Time")
     plt.ylabel("Amplitude")
