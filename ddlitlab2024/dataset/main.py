3ae145b9c78113944920a0208e3ff3c29effef1d
texhnolyze <joegriepen@gmail.com>
2024-11-14 11:27:09 +0100

    feat(import): setup initial rosbag import
    of gamestate, joint states and joint commands

--- ddlitlab2024/dataset/main.py
+++ ddlitlab2024/dataset/main.py
@@ -12,8 +12,9 @@ from rich.console import Console
 
 from ddlitlab2024 import __version__
 from ddlitlab2024.dataset import logger
-from ddlitlab2024.dataset.cli import CLIArgs, CLICommand
+from ddlitlab2024.dataset.cli import CLIArgs, CLICommand, ImportType
 from ddlitlab2024.dataset.db import Database
+from ddlitlab2024.dataset.mappers.rosbag_mapper import RosBagToModelMapper
 
 err_console = Console(stderr=True)
 
@@ -37,13 +38,17 @@ def main():
             match args.db_command:
                 case "recording2mcap":
                     from ddlitlab2024.dataset.recording2mcap import recording2mcap
+
                     recording2mcap(db.session, args.recording, args.output_dir)
 
                 case "dummy-data":
                     from ddlitlab2024.dataset.dummy_data import insert_dummy_data
+
                     insert_dummy_data(db.session, args.num_recordings, args.num_samples_per_rec, args.image_step)
 
-                    insert_dummy_data(db, args.num_recordings, args.num_samples_per_rec, args.image_step)
+        elif args.type == ImportType.ROS_BAG:
+            db: Database = Database(args.db_path).create_session()
+            RosBagToModelMapper(args.file, db).read()
 
         sys.exit(0)
     except Exception as e:
--- ddlitlab2024/utils/utils.py
+++ ddlitlab2024/utils/utils.py
@@ -1,5 +1,9 @@
+import re
+
 import numpy as np
 from transforms3d.quaternions import quat2axangle
+
+CAMELCASE_TO_SNAKECASE_REGEX = re.compile(r"(?<!^)(?=[A-Z])")
 
 
 def quats_to_5d(quats: np.ndarray) -> np.ndarray:
@@ -38,3 +42,24 @@ def wxyz2xyzw(quat: np.ndarray) -> np.ndarray:
     :return: The (xyzw) representation.
     """
     return np.roll(quat, -1, axis=-1)
+
+
+def timestamp_in_ns(seconds: int, nanoseconds: int) -> int:
+    """
+    Convert a combined unix timestamp from seconds and nanoseconds to timestamp in nanoseconds.
+    """
+    return int(seconds * 1e9) + nanoseconds
+
+
+def timestamp_in_s(seconds: int, nanoseconds: int) -> float:
+    """
+    Convert a timestamp in nanoseconds to a combined unix timestamp in seconds as float.
+    """
+    return seconds + nanoseconds / 1e9
+
+
+def camelcase_to_snakecase(name: str) -> str:
+    """
+    Convert a camelCase string to snake_case.
+    """
+    return CAMELCASE_TO_SNAKECASE_REGEX.sub("_", name).lower()
