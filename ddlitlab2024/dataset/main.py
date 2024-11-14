c9cbcb0c281db71a623925645bc47e66459d9287
texhnolyze <joegriepen@gmail.com>
2024-11-14 11:24:40 +0100

    feat(db): turn into a fluent API
    and close DB session on destruction of object

--- ddlitlab2024/dataset/main.py
+++ ddlitlab2024/dataset/main.py
@@ -9,7 +9,6 @@ import os
 import sys
 
 from rich.console import Console
-from sqlalchemy.orm import Session
 
 from ddlitlab2024 import __version__
 from ddlitlab2024.dataset import logger
@@ -25,7 +24,7 @@ def main():
     try:
         logger.debug("Parsing CLI args...")
         args: Namespace = CLIArgs().parse_args()
-        logger.debug(f"CLI args: {args}")
+        logger.debug(f"Parsed CLI args: {args}")
 
         if args.version:
             logger.info(f"running ddlitlab2024 CLI v{__version__}")
@@ -33,17 +32,16 @@ def main():
 
         if args.command == CLICommand.DB:
             create_schema = args.db_command == "create-schema" or args.db_command == "dummy-data"
-            db: Session = Database(args.db_path).create_session(create_schema=create_schema)
-            logger.debug("Database session created")
+            db: Database = Database(args.db_path).create_session(create_schema=create_schema)
 
             match args.db_command:
                 case "recording2mcap":
                     from ddlitlab2024.dataset.recording2mcap import recording2mcap
-
-                    recording2mcap(db, args.recording, args.output_dir)
+                    recording2mcap(db.session, args.recording, args.output_dir)
 
                 case "dummy-data":
                     from ddlitlab2024.dataset.dummy_data import insert_dummy_data
+                    insert_dummy_data(db.session, args.num_recordings, args.num_samples_per_rec, args.image_step)
 
                     insert_dummy_data(db, args.num_recordings, args.num_samples_per_rec, args.image_step)
 
