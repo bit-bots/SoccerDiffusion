aa52eea80a53a73ffa0482bf48dc1f9e7eae7c69
Jasper <jasper.gueldenstein@uni-hamburg.de>
2025-04-21 22:31:08 +0200

    gh page initial

--- ddlitlab2024/dataset/errors.py
+++
@@ -1,2 +0,0 @@
-class CLIArgumentError(Exception):
-    """Raised when the configuration of CLI arguments is not valid and execution is impossible"""
--- ddlitlab2024/ml/model/__init__.py
+++
@@ -1,3 +0,0 @@
-from .model import End2EndDiffusionTransformer
-
-__all__ = ["End2EndDiffusionTransformer"]
